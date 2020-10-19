2fa1a9e3dea206cad0c57210bbbda0c3bf249f8a
Bong <zzong2006@gmail.com>
2020-10-20 02:12:06 +0900

    Add C++ Language

---
+++ C++Project/cmake-build-debug/CMakeFiles/C__Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Project.dir/main.cpp.obj"
+  "C__Project.exe"
+  "C__Project.exe.manifest"
+  "C__Project.pdb"
+  "libC__Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
